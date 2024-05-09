017ef399c878c8ec57252fb93d7ed4d49751f02c
Totengeist <me@totengeist.com>
2024-05-09 11:37:54 -0700

    attraction: Use an embed

--- attraction/attraction.py
+++ attraction/attraction.py
@@ -1,8 +1,10 @@
-from redbot.core import commands
-import toml
 import os
-import random
 import pathlib
+import random
+import toml
+
+from redbot.core import commands
+import discord
 
 class Attraction(commands.Cog):
     """Retrieve a random animal attraction in the San Diego area."""
@@ -17,4 +19,22 @@ class Attraction(commands.Cog):
         path = os.path.join(pathlib.Path(__file__).parent.resolve(), 'attractions.toml')
         data = toml.load(path)
         item = random.choice(data['attractions'])
-        await ctx.send(item["name"])
+        embed = discord.Embed(title=item["name"], description=item["description"], url=item["website"])
+
+        if "address" in item.keys():
+            embed.add_field(name="Address", value=item["address"], inline=False)
+
+        parking = self._descr_or_url(item, "parking", "parking_url")
+        if parking != "":
+            embed.add_field(name="Parking", value=parking, inline=False)
+        await ctx.send(embed=embed)
+
+    def _descr_or_url(self, data, description, url):
+        value = ""
+        if description in data.keys() and url in data.keys():
+            value = data[description]+ " \\[[more information]("+data[url]+")\\]"
+        elif description in data.keys():
+            value = data[description]
+        elif url in data.keys():
+            value = "\\[[view online]("+data[url]+")\\]"
+        return value
